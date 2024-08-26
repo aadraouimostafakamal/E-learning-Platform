76a80ab26bc1be70b1e6c1d930e6a7af10325acf
aadraouimostafakamal <mostafa-kamal.aadraoui23@ump.ac.ma>
2024-08-26 01:43:05 +0100

    supabaseClient modification

--- app/utils/supabaseClient.js
+++ app/utils/supabaseClient.js
@@ -5,7 +5,6 @@ const { createClient } = require('@supabase/supabase-js');
 // Use environment variables for Supabase credentials
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-console.log(supabaseUrl, supabaseKey);
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Missing Supabase URL or Key from environment variables");
